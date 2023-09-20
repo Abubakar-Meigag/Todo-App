3ce54ddf7fa77ed3c1982dddb5479e58446b6b9e
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-20 22:07:59 +0100

    done with input todo component

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useState } from "react";
+
+function InputTodo() {
+    const [description, setDescription] = useState("");
+    const handelChange = (e) => setDescription(e.target.value);
+
+    const sendTodo = async (e) => {
+        e.preventDefault()
+        try {
+            const body = { description }
+            const res = await fetch("http://localhost:5099/todo", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            })
+            console.log(res);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Beko Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={sendTodo}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={handelChange}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+}
+
+export default InputTodo;
